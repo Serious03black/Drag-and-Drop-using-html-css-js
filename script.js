ceefda2ec5cd84de02cd07955751d59f3c66338f
serious03bleck <prathaamesh2509@gmail.com>
2025-01-10 11:52:56 +0530

    add more feature

--- script.js
+++
@@ -1,9 +0,0 @@
-const dropArea=document.getElementById("#drop-area")
-const inputFile=document.getElementById("#input-file")
-const imageView=document.getElementById(".Image-View")
- inputFile.addEventListener("change",uploadImage)
- function uploadImage(){
-    var imgLInk = URL.createObjectURL(inputFile.files[0])
-    imageView.style.background = `url(${imgLInk})`;
-
- }
